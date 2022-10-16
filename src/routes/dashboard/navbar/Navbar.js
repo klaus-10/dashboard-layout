0ef635475d54261259033edb2e25fe73d5940ad8
klaus-10 <k.shala@campus.unimib.it>
2022-10-16 23:50:27 +0200

    Laout improve

--- src/routes/dashboard/navbar/Navbar.js
+++ src/routes/dashboard/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
 import Popover from "@mui/material/Popover";
@@ -10,6 +10,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import "./Navbar.css";
 import { Divider } from "@mui/material";
+import { UserContex } from "../../../UserContext";
 
 export default function Navbar(props) {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -25,132 +26,183 @@ export default function Navbar(props) {
   const open = Boolean(anchorEl);
   const id = open ? "simple-popover" : undefined;
 
+  // context
+  const user = useContext(UserContex);
+
   return (
     <div className={props.matches ? "navbar extended" : "navbar"}>
       {props.mediaQy ? (
-        <div className="search-box">
+        <div className="search-box" style={{ cursor: "pointer" }}>
           <SearchIcon />
         </div>
       ) : (
-        <div onClick={props.openSide}>{props.side ? "closed" : "open"}</div>
+        <div style={{ cursor: "pointer" }} onClick={props.openSide}>
+          {props.side ? "closed" : "open"}
+        </div>
       )}
 
       <Stack direction="row" spacing={2}>
-        <Avatar sx={{ bgcolor: "transparent" }}>
+        <Avatar sx={{ bgcolor: "transparent", cursor: "pointer" }}>
           <NotificationsIcon />
         </Avatar>
         <Avatar
           alt="Remy Sharp"
           src="/static/images/avatar/1.jpg"
+          sx={{ cursor: "pointer" }}
           onClick={handleClick}
         />
       </Stack>
 
-      <Popover
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "left",
-        }}
-        style={{ top: "5px" }}
-      >
-        <Typography
-          style={{
-            fontSize: "13px",
-            cursor: "pointer",
-            padding: "0px 20px",
-            paddingTop: "10px",
-            paddingBottom: "5px",
-            fontWeight: "bold",
+      {user ? (
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
           }}
+          style={{ top: "5px" }}
         >
-          Admin
-        </Typography>
-        <Typography
-          style={{
-            fontSize: "13px",
-            cursor: "pointer",
-            paddingLeft: "20px",
-            paddingRight: "50px",
-            paddingTop: "5px",
-            paddingBottom: "10px",
-          }}
-        >
-          admin@gmail.com
-        </Typography>
-        <Divider />
-        <div style={{ padding: "10px 5px" }}>
-          <div style={{ padding: "0px 0px" }}>
-            <Typography
-              style={{
-                fontSize: "13px",
-                cursor: "pointer",
-                paddingLeft: "20px",
-                paddingRight: "50px",
-                paddingTop: "6px",
-                paddingBottom: "6px",
-                borderRadius: "5px",
-              }}
-              className="textHover"
-            >
-              Home
-            </Typography>
-          </div>
-          <div style={{ padding: "0px 0px" }}>
-            <Typography
-              style={{
-                fontSize: "13px",
-                cursor: "pointer",
-                paddingLeft: "20px",
-                paddingRight: "50px",
-                paddingTop: "6px",
-                paddingBottom: "6px",
-                borderRadius: "5px",
-              }}
-              className="textHover"
-            >
-              Profile
-            </Typography>
-          </div>
-          <div style={{ padding: "0px 0px" }}>
-            <Typography
-              style={{
-                fontSize: "13px",
-                cursor: "pointer",
-                paddingLeft: "20px",
-                paddingRight: "50px",
-                paddingTop: "6px",
-                paddingBottom: "6px",
-                borderRadius: "5px",
-              }}
-              className="textHover"
-            >
-              Settings
-            </Typography>
-          </div>
-        </div>
-
-        <Divider />
-        <div style={{ padding: "10px 5px" }}>
+          <Typography
+            style={{
+              fontSize: "13px",
+              cursor: "pointer",
+              padding: "0px 20px",
+              paddingTop: "10px",
+              paddingBottom: "5px",
+              fontWeight: "bold",
+            }}
+          >
+            Admin
+          </Typography>
           <Typography
             style={{
               fontSize: "13px",
               cursor: "pointer",
               paddingLeft: "20px",
               paddingRight: "50px",
-              paddingTop: "6px",
-              paddingBottom: "6px",
-              borderRadius: "5px",
+              paddingTop: "5px",
+              paddingBottom: "10px",
             }}
-            className="textHover"
           >
-            Logout
+            admin@gmail.com
           </Typography>
-        </div>
-      </Popover>
+          <Divider />
+          <div style={{ padding: "10px 5px" }}>
+            <div style={{ padding: "0px 0px" }}>
+              <Typography
+                style={{
+                  fontSize: "13px",
+                  cursor: "pointer",
+                  paddingLeft: "20px",
+                  paddingRight: "50px",
+                  paddingTop: "6px",
+                  paddingBottom: "6px",
+                  borderRadius: "5px",
+                }}
+                className="textHover"
+              >
+                Home
+              </Typography>
+            </div>
+            <div style={{ padding: "0px 0px" }}>
+              <Typography
+                style={{
+                  fontSize: "13px",
+                  cursor: "pointer",
+                  paddingLeft: "20px",
+                  paddingRight: "50px",
+                  paddingTop: "6px",
+                  paddingBottom: "6px",
+                  borderRadius: "5px",
+                }}
+                className="textHover"
+              >
+                Profile
+              </Typography>
+            </div>
+            <div style={{ padding: "0px 0px" }}>
+              <Typography
+                style={{
+                  fontSize: "13px",
+                  cursor: "pointer",
+                  paddingLeft: "20px",
+                  paddingRight: "50px",
+                  paddingTop: "6px",
+                  paddingBottom: "6px",
+                  borderRadius: "5px",
+                }}
+                className="textHover"
+              >
+                Settings
+              </Typography>
+            </div>
+          </div>
+
+          <Divider />
+          <div style={{ padding: "10px 5px" }}>
+            <Typography
+              style={{
+                fontSize: "13px",
+                cursor: "pointer",
+                paddingLeft: "20px",
+                paddingRight: "50px",
+                paddingTop: "6px",
+                paddingBottom: "6px",
+                borderRadius: "5px",
+              }}
+              className="textHover"
+            >
+              Logout
+            </Typography>
+          </div>
+        </Popover>
+      ) : (
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          style={{ top: "5px" }}
+        >
+          <Typography
+            style={{
+              fontSize: "13px",
+              cursor: "pointer",
+              padding: "0px 20px",
+              paddingTop: "10px",
+              paddingBottom: "5px",
+              fontWeight: "bold",
+            }}
+          >
+            Profile
+          </Typography>
+
+          <Divider />
+          <div style={{ padding: "10px 5px" }}>
+            <Typography
+              style={{
+                fontSize: "13px",
+                cursor: "pointer",
+                paddingLeft: "20px",
+                paddingRight: "50px",
+                paddingTop: "6px",
+                paddingBottom: "6px",
+                borderRadius: "5px",
+              }}
+              className="textHover"
+            >
+              Login
+            </Typography>
+          </div>
+        </Popover>
+      )}
     </div>
   );
 }
--- src/routes/dashboard/sidebar/Sidebar.js
+++ src/routes/dashboard/sidebar/Sidebar.js
@@ -1,18 +1,83 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import Avatar from "@mui/material/Avatar";
+// import Icon from "../../../components/Icon";
+import { Icon } from "@mui/material/";
+
+// import * as MuiIcons from "@mui/icons-material";
+import InboxIcon from "@mui/icons-material/Inbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
+
 import elements from "./Sidebar.json";
 
 import "./Sidebar.css";
+import { ListItem } from "@mui/material";
 
 export default function Sidebar(props) {
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
+
   return (
     <div className={props.open === true ? "sidebar open" : "sidebar close"}>
       {/* {!props.mediaQy && <p onClick={props.close}>Close ICON</p>} */}
-      <h1>Sidebar</h1>
+      <Box
+        sx={{
+          width: "100%",
+          maxWidth: 360,
+        }}
+      >
+        <ListItem className="sidebar-logo">
+          <ListItemIcon className="sidebar-logo-icon">
+            <InboxIcon className="slide-icon" />
+          </ListItemIcon>
+        </ListItem>
 
-      {elements.sidebar.map((el) => (
-        <div>
-          <h5>{el}</h5>
+        <div style={{ padding: "10px 20px" }}>
+          <ListItem className="sidebar-logo-admin">
+            <Avatar sx={{ bgcolor: "#637b92" }}>
+              {/* <NotificationsIcon /> */}A
+            </Avatar>
+            <ListItemText className="slide-admin-text" primary="Admin" />
+          </ListItem>
         </div>
-      ))}
+        <List component="nav" aria-label="main mailbox folders">
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+
+          {elements.sidebar.map((el, index) => (
+            // <div>
+            //   <h5>{el}</h5>
+            // </div>
+            <ListItemButton
+              key={index}
+              selected={selectedIndex === index}
+              onClick={(event) => handleListItemClick(event, index)}
+            >
+              <ListItemIcon>
+                {/* <InboxIcon className="slide-icon" /> */}
+                {/* <FontAwesomeIcon className="slide-icon" icon={el?.icon}/> */}
+                {/* <Icon icon={el.name} /> */}
+                <Icon
+                  fontSize="small"
+                  className={selectedIndex === index ? "googleIconColor" : ""}
+                >
+                  {el.icon}
+                </Icon>
+                {/* <Icon fontSize="small">add_circle</Icon> */}
+              </ListItemIcon>
+              <ListItemText className="slide-text" primary={el.text} />
+            </ListItemButton>
+          ))}
+        </List>
+      </Box>
     </div>
   );
 }
